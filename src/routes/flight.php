957ed550cf2a63b4d634c26c246ead5227b4c21a
Adigun Jide Idris <edijman@gmail.com>
2018-11-09 12:45:17 -0500

    Update src/routes/flight.php
    Updated flight query to cover additiona time period in a day

--- src/routes/flight.php
+++ src/routes/flight.php
@@ -72,7 +72,7 @@ function getAirline($name){
 }
 function getFlight($departCityCode, $arriveCityCode, $departureDate){
 
-    $sql = "SELECT * FROM trip.Flights as flight WHERE `departure_airport` = '$departCityCode' AND `arrival_airport` = '$arriveCityCode' AND DATE(departure_time) = '$departureDate'"; 
+    $sql = "SELECT * FROM trip.Flights as flight WHERE `departure_airport` = '$departCityCode' AND `arrival_airport` = '$arriveCityCode' AND `departure_time` >= '$departureDate' AND `departure_time` < ('$departureDate' + INTERVAL 1 DAY)"; 
     try{
         $db = new db();
         $db = $db->connect();
