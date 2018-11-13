1cc845c2cb345e3232796e7bb06aed1657a341e3
Adigun Jide Idris <edijman@gmail.com>
2018-11-13 11:47:30 -0500

    Update src/routes/flight.php

--- src/routes/flight.php
+++ src/routes/flight.php
@@ -11,29 +11,30 @@ $app->get('/itinerary/{departure_city_id}/{arrival_city_id}/{departureDate}', fu
     $departure_city_id = $args['departure_city_id'];
     $arrival_city_id = $args['arrival_city_id'];
     $departureDate = $args['departureDate'];
+
     $db = new db();
     $db = $db->connect();
 
-    $airport = new Airport();
-    $flightO = new Flight();
+    $airportObj = new Airport();
+    $flightObj = new Flight();
 
     //get airport in the cities base on the city id
-    $arriveCityCode = $airport->getCode($db, $arrival_city_id, 'arrival');
-    $departCityCode = $airport->getCode($db, $departure_city_id, 'departure');
+    $arriveCityCode = $airportObj->getCode($db, $arrival_city_id, 'arrival');
+    $departCityCode = $airportObj->getCode($db, $departure_city_id, 'departure');
     
     //get Flight based on departure and arrival city code
     $flight = $flightObj->getDirectFlight($db, $departCityCode, $arriveCityCode, $departureDate);
-    $airline =[];
-
+    
     //Get the airline for each flight
+    $airline =[];
     for($i = 0; $i < count($flight); $i++ )
     {
         $airline = array_merge($airline, $flightObj->getAirline($db, $flight[$i]['airline']));
         $airline = array_unique($airline, SORT_REGULAR);
     }
 
-    $departureAirport = $airport->getAirport($db, $departCityCode);
-    $arrivalAirport = $airport->getAirport($db, $arriveCityCode);
+    $departureAirport = $airportObj->getAirport($db, $departCityCode);
+    $arrivalAirport = $airportObj->getAirport($db, $arriveCityCode);
     $airports = array_merge($departureAirport, $arrivalAirport);
 
     $res->getBody()->write( '
